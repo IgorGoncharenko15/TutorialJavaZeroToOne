a745ceaa04311d19e0ba9a7901182018c872aaa9
igor <igor@igor>
2019-04-22 17:13:01 +0300

    added db run file

---
+++ src/pkgMain/run_db.java
@@ -0,0 +1,13 @@
+package pkgMain;
+
+public class run_db {
+
+    public static void main(String[] args) {
+        
+        
+        
+        
+        System.out.println("Done!");
+    }
+    
+}
